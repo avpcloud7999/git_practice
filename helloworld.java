b8d72ff6c7302f25fe19f3fa253a865ec8e6e04b
avpcloud7999 <avpcloud7999@gmail.com>
2025-11-24 16:45:04 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello, World in Jenkins today is 24th november");
+        System.out.println("Hello, World in Jenkins today is 24th november and run automatically through jenkins triggers");
     }
 }
